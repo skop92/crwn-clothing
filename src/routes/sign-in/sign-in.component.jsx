c32118060e8ec2100457a0b0fcb60f9219748743
Samu Koponen <ab5154@student.jamk.fi>
2023-05-08 19:42:27 +0300

    Sign in with redirect, may be removed

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,29 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
+import {
+  auth,
+  signInWithGooglePopup,
+  signInWithGoogleRedirect,
+  createUserDocumentFromAuth
+} from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+  useEffect(() => {
+    async function handleRedirect() {
+      // Some browser settings may prevent this from working as expected
+      // ... like settings related to sessionStorage
+      const response = await getRedirectResult(auth);
+      if(response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+      }
+      //console.log(response);
+    }
+    handleRedirect();
+  }, []);
+
   const logGoogleUser = async () => {
-    //const response = await signInWithGooglePopup();
-    //console.log(response);
-    const { user } = await signInWithGooglePopup();
+    const { user } = await signInWithGooglePopup(); // destruct from response
     const userDocRef = await createUserDocumentFromAuth(user);
   }
 
@@ -13,6 +32,9 @@ const SignIn = () => {
       <h1>Sign in page</h1>
       <button onClick={logGoogleUser}>
         Sign in with Google Popup
+      </button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
       </button>
     </div>
   )
