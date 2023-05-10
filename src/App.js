24f76bc1f58981df1b0fc71f377a399dc21c07df
Samu Koponen <samu.j.koponen@gmail.com>
2023-05-10 20:31:41 +0300

    Example products on shop page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
-
-const Shop = () => {
-  return (
-    <div>
-      <p>Shop now</p>
-    </div>
-  )
-}
+import Shop from './routes/shop/shop.component';
 
 const App = () => {
   return (
---
+++ src/contexts/products.context.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState, useEffect } from 'react';
+import SHOP_DATA from '../shop-data.json';
+
+export const ProductsContext = createContext({
+  products: [],
+  setProducts: () => [],
+});
+
+export const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  const value = { products, setProducts };
+
+  useEffect(() => {
+    setProducts(SHOP_DATA);
+  }, []);
+
+  return <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
+}
