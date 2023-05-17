698a5ec358a62b9de946de52b993739c743860dd
Samu Koponen <samu.j.koponen@gmail.com>
2023-05-17 16:30:19 +0300

    Reselect with category selector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,18 @@
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((acc, category) => {
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+// This does not run the callback if selectCategoryReducer has not changed
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => categories.reduce((acc, category) => {
     const { title, items } = category;
     acc[title.toLowerCase()] = items;
     return acc;
-  }, {});
+  }, {})
+);
